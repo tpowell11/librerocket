5980ff57e828954eaa738dee8dae4721d79f35ad
Tom Powell <thomastruittpowell@gmail.com>
2021-03-04 09:39:40 -0500

    c

--- src/diagramparts.py
+++ src/diagramparts.py
@@ -2,9 +2,13 @@ from tkinter import *
 
 def drawTube(canvas: Canvas ,length: float, diameter: float):
     "Draws a bodytube to a tk canvas"
-    canvas.create_polygon((10, 50, 240, 210),fill='red')
-def drawEllipticCone(length: float, baseod: float):
+    width, height = int(canvas.cget('width')), int(canvas.cget('height'))
+    canvas.create_rectangle((height/2)+10, (height/2)-10, (width/2)+100, (width/2)-100, width=5, fill='red')
+def drawEllipticCone(canvas: Canvas, length: float, baseod: float):
     "Draws an elliptic nosecone to a tk canvas"
     
-def drawConicalCone(length: float, baseod: float):
+def drawConicalCone(canvas: Canvas, length: float, baseod: float):
     "Draws an conical nosecone to a tk convas"
+
+def drawOgiveCone(canvas: Canvas, length: float, baseod: float):
+    "Draws an ogive nosecone to the canvas"
