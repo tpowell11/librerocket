35b7d3b9a935e95297cc8a02b1fe4147953e4bc6
Tom Powell <thomastruittpowell@gmail.com>
2021-03-13 20:19:26 -0700

    cleanup & documentation

--- src/rocket.py
+++ src/rocket.py
@@ -10,22 +10,28 @@ class Rocket(object):
     def SaveJson(self, path:str):
         data={
             "filename":self.filename,
+            "type":__name__,
             "parts":[]
         }
         for item in self.parts:
             data['parts'].append(item.getData()) #add each component to json parts list
-        print(data)
         with open(str(path),'w+') as f:
             f.write(json.dumps(data,indent=4))
             f.close()
-            
+def loadJsontoObject(filename:str) -> Rocket:
+    "loads a json file and returns the proper objects"
+    with open(filename,"r") as f: #open file
+        data = json.load(f) #read file
+        for key in data['parts']:
+            print(key['type'])
 class fileParent(object):
     "The top level class for the file"
     def __init__(self,Name: str):
         self.name = Name
     def getData(self) -> str:
         return {
-            'name':self.name
+            'name':self.name,
+            'type':__name__
         }
 class stage(object):
     "class for rocket stages"
@@ -41,13 +47,14 @@ class component(object):
     material: str #refers to a material in materials.json
     parent = '' #contains the parent for a component, used in rendering treeview 
 class motor(component):
-    objtype='motor'
+    objtype=__name__
     "Fields for motors"
-    def __init__(self,Name: str, Itot: float, Diameter: float, Mass: float, Length: float, Curve: dict, Parent = ''):
+    def __init__(self,Name: str, Itot: float, Diameter: float, Mass: float, Length: float, Curve: dict,Material: str, Parent = ''):
         "Moves data into the correct areas"
         self.name = Name
         self.parent = Parent
         self.Itot = Itot
+        self.material = Material
         self.diameter = Diameter
         self.mass = Mass
         self.length = Length
@@ -56,12 +63,13 @@ class motor(component):
         return {
             "name":self.name,
             "parent":self.parent,
-            "type":'motor',
+            "type":__name__,
             "data":{
                 "mass":self.mass,
                 "length":self.length,
                 "diameter":self.diameter,
                 "length":self.length,
+                "material":self.material,
                 "Itot":self.Itot,
             }
         }
@@ -78,8 +86,8 @@ class tube(component):
     def getData(self):
         return {
             "name":self.name,
+            "type":__name__,
             "parent":self.parent,
-            "type":'tube',
             "data":{
                 "mass":self.mass,
                 "length":self.length,
@@ -95,6 +103,19 @@ class nosecone(component):
         self.length = Length
         self.shoulder = Shoulder
         self.shoulderdiameter = ShoulderDiameter
+    def getData(self):
+        return {
+            "name":self.name,
+            "type":__name__,
+            "parent":self.parent,
+            "data":{
+                "mass":self.mass,
+                "length":self.length,
+                "diameter":self.diameter,
+                "generator":self.generator,
+                "shoulder":self.shoulder
+            }
+        }
 class trapfins(component):
     "automatc trapesoidal fins"
 class ellipfins(component):
