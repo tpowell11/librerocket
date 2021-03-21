3bdbce0bd793ea00ec3bd4cc3199fbfc29691d9f
Tom Powell <thomastruittpowell@gmail.com>
2021-03-20 22:10:31 -0400

    added ability to parse rocksim .rkt files

--- src/rocketfile.py
+++ src/rocketfile.py
@@ -1,19 +1,21 @@
 import lxml
 import json
 import zipfile
+import rocket
 from io import bytes, TextIOWrapper
 class orkFile(object):
     """contains and manipulates OpenRocket (<=15.03) files
     """
-    def openfile(filepath: str)->bytes or TextIOWrapper:
+    def openfile(self, filepath: str)->bytes or TextIOWrapper:
         "gets the IO object for a given file, should not be called outside of this class"
+        self.filepath = filepath
         try:
             zf = zipfile.ZipFile(filepath,mode='r') #declare the actual file binary
             return zf.open('rocket.ork') #open the internal file from the zip
         except zipfile.BadZipFile:
             return open(filepath,'r')
             
-    def loadfile(self,filepath: str)->list[dict]:
+    def parseORK(self,filepath: str)->list[dict]:
         """
         Loads both direct xml ork's and zipped xml ork (the default in openrocket)
         It returns a list of dicts. To find the type of the component that the values came from,
@@ -66,5 +68,59 @@ class orkFile(object):
             if element.tag == 'point':
                 m[-1][-1][element.tag]={'x':element.get('x'),'y':element.get('y')} #group finpoints with their parent
         self.components=m
+        self.importedfiletype='ork15'
+    def parseRKT(self,filepath:str)->list[dict]:
+        m=[]
+        has_attributes = [
+            'BodyTube','Ring','LaunchLug','FinSet','MassObject','Streamer','Parachute','NoseCone','Transition'
+        ]
+        is_value_float = [
+            'KnownMass','Density','KnownCG','Xb','CalcMass','CalcCG','RadialLoc','RadialAngle','Len','WallThickness','BaseDia',
+            'ShoulderLen','ShoulderOD','Dia','DragCoefficient','ShroudLineLength','ShroudLineMassPerMM','Width',
+            'TabLength','TabDepth','TabOffset','RootChord','TipChord','SemiSpan','SweepDistance','CantAngle','OD','ID',
+            'EngineOverhang','MotorDia','FrontShoulderLen','RearShoulderLen','FrontShoulderDia','RearShoulderDia','FrontDia',
+            'RearDia'
+        ]
+        is_value_int = [
+            'DensityType','LocationMode','FinishCode','SerialNo','ShapeCode','ConstructionType','ChuteCount','FinCount',
+            'TipShapeCode','UsageCode'
+        ]
+        is_value_str = [
+            'Material','ShroudLineMaterial'
+        ]
+        is_value_bool = [
+            'UseKnownCG','IsMotorMount','IsInsideTube','AutoSize'
+        ]
+        f=open(filepath,'r')
+        root=lxml.etree.parse(f)
+        for element in root.iter('*'):
+            if element.tag in has_attributes:
+                m.append({'cmptype':element.tag}) #append the part's name to the list
+            #general processing for elements whose data is stored in element text
+            if element.tag in is_value_float:
+                m[-1][element.tag]=float(element.text)
+            if element.tag in is_value_str: #make the element's payload a float
+                m[-1][element.tag]=str(element.text)
+            if element.tag in is_value_bool: #make the element's payload a bool
+                m[-1][element.tag]=bool(element.text)
+            if element.tag == 'PointList': #converts the <PointList>50,0|75.0,50.0|37,12|0.0,0.0|</PointList> to list[tuple]
+                points = []
+                try: 
+                    for pair in t.split('|'): #split out each pair
+                        j =tuple(map(float,pair.split(','))) #map each number to float type
+                        points.append(j)
+                except: #for .split's insistance on inserting a [...,''] in lists
+                    pass
+                m[-1][element.tag]=points
+            self.components = m
+            self.importedfiletype = 'rkt'
     def getJson(self):
         return json.dumps(self.components)
+    def getRocket(self)-> rocket.Rocket:
+        "gets a Rocket object from openrocket xml or rocksim xml"
+        if self.importedfiletype == 'ork15':
+            for i in range(0,len(self.components)):
+                cmptype = self.components[i]['cmptype']
+        elif self.importedfiletype == 'rkt':
+            pass
+            
